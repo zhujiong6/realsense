8da6c267bedad8d79d633e541ed49e2cd5f74cb5
zhujiong <zhujiong6@hotmail.com>
2016-06-07 21:56:46 +0800

    速度加速度尝试。。但是似乎失败了

---
+++ FF_HandsViewer.cs/Program.cs
@@ -0,0 +1,38 @@
+﻿/*******************************************************************************
+
+INTEL CORPORATION PROPRIETARY INFORMATION
+This software is supplied under the terms of a license agreement or nondisclosure
+agreement with Intel Corporation and may not be copied or disclosed except in
+accordance with the terms of that agreement
+Copyright(c) 2013 Intel Corporation. All Rights Reserved.
+
+*******************************************************************************/
+using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hands_viewer.cs
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            PXCMSession session = null;
+            session = PXCMSession.CreateInstance();
+            if (session != null)
+            {
+                Application.Run(new MainForm(session));
+                session.Dispose();
+            }
+        }
+    }
+}
